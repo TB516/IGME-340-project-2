bfb92ca29efce38ce282a5088e68103f3eed20c4
Thomas <77514919+TB516@users.noreply.github.com>
2025-04-09 06:43:26 -0400

    Added about page

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Elden Ring Spell Viewer',
       theme: ThemeData(colorScheme: ColorScheme.dark()),
+      debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
@@ -22,6 +23,36 @@ class MyApp extends StatelessWidget {
             bottom: TabBar(
               tabs: [Tab(text: 'Sorceries'), Tab(text: 'Incantations')],
             ),
+            actions: [
+              Builder(
+                builder: (context) {
+                  return IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Elden Ring Spell Viewer'),
+                            content: const SingleChildScrollView(
+                              child: Text('''
+The process of creating the app went pretty smoothly for the most part. I started off with the controls, then moved over to the cards, then wrapped everything up with the favorites feature. I did encounter some errors which prolonged my dev time a fair bit, but they weren't apart of the app itself. They were actually related to the API I was using. The issues were due to bad data for some image urls. As I'm the owner of that API, I was able to find out the issue once I realized it was an API issue, so that was good at least. For meeting the requirements, on either of the main screens, you can see multiple form controls, and there aren't many errors to encounter as the main search is a filter instead of a direct search. Overall I think the app turned out pretty nice, though I think a separate section for the bookmarks could be useful and tuning the styling couldn't hurt.
+                            '''),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    icon: const Icon(Icons.info),
+                  );
+                },
+              ),
+            ],
           ),
           body: TabBarView(
             children: [
